eb0a8cc66d7ba2f4ae492cc7ecfce01d01cdbd37
wolver922 <leo0938278619@gmail.com>
2018-07-19 12:38:27 +0800

    finish hw1

--- HW1/HW1/processString.cpp
+++ HW1/HW1/processString.cpp
@@ -16,10 +16,24 @@ using namespace std;
 //       -1 if exception occur (ex. string containing non-digit character)
 int getAscendingStr(string& inputStr)
 {
-	
 	/// Please fill your code here
+	istringstream iss(inputStr);
+	istringstream judg_iss(inputStr);
+	stringstream result;
 
+	vector<float> nums;
+	vector<string> strs;
+	copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(strs));
+	copy(istream_iterator<float>(judg_iss), istream_iterator<float>(), back_inserter(nums));
 
+	//成立表示轉數字部份有發生轉換失敗(EOF)
+	if (nums.size() != strs.size())
+		return -1;
+	std::sort(nums.begin(), nums.end());
+	std::copy(nums.begin(), nums.end(), std::ostream_iterator<float>(result, " "));
+	result.str().erase(result.str().find_last_not_of(" ") + 1);
+	std::cout << result.str().erase(result.str().find_last_not_of(" ") + 1) << endl;
+	inputStr = result.str().erase(result.str().find_last_not_of(" ") + 1);
 	return 0;
 }
 
@@ -36,8 +50,21 @@ int getAscendingStr(string& inputStr)
 //          (return vector size should be 0)
 int solveQ(vector<double> &x, double a, double b, double c)
 {
+		if (b*b - 4 * a*c > 0) {
 
-	return 0;
+		x.push_back((-b + sqrt(b*b - 4 * a*c)) / (2 * a));
+		x.push_back((-b - sqrt(b*b - 4 * a*c)) / (2 * a));
+		return (1);
+	}
+	else if (b*b - 4 * a*c == 0) {
+		x.push_back((-b) / (2 * a));
+		return (0);
+	}
+	else {
+		// ( b*b - 4*a*c IS < 0 )
+		// complex roots
+		return (-1);
+	}
 }
 
 int main(int argc, char*argv[]) {
